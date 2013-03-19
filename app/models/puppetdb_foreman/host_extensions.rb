e7ea862202281c18bac873cbb22f4136a01cef0f
Daniel Lobato <elobatocs@gmail.com>
2013-03-19 11:49:29 +0100

    Updated gemspec and puppetdb deactivation

--- app/models/puppetdb_foreman/host_extensions.rb
+++ app/models/puppetdb_foreman/host_extensions.rb
@@ -1,4 +1,39 @@
 module PuppetdbPlugin
   module Callbacks
+    included do
+      after_destroy :deactivate_host
+
+      private
+
+      def deactivate_host
+        logger.debug "Deactivating host #{name} in Puppetdb"
+        if SETTINGS[:puppetdb][:enabled] && SETTINGS[:puppetdb][:address].present?
+          begin
+            uri = URI.parse(SETTINGS[:puppetdb][:address])
+            req = Net::HTTP::Post.new(uri.path)
+            req['Accept'] = 'application/json'
+            req.set_form_data({"command":"deactivate node","version":1,"payload":"\"#{name}\""})
+            res             = Net::HTTP.new(uri.host, uri.port)
+            res.use_ssl     = uri.scheme == 'https'
+            if res.use_ssl?
+              if SETTINGS[:ssl_ca_file]
+                res.ca_file = SETTINGS[:ssl_ca_file]
+                res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+              else
+                res.verify_mode = OpenSSL::SSL::VERIFY_NONE
+              end
+              if SETTINGS[:ssl_certificate] && SETTINGS[:ssl_private_key]
+                res.cert = OpenSSL::X509::Certificate.new(File.read(SETTINGS[:ssl_certificate]))
+                res.key  = OpenSSL::PKey::RSA.new(File.read(SETTINGS[:ssl_private_key]), nil)
+              end
+            end
+            res.start { |http| http.request(req) }
+          rescue => e
+            raise "Could not deactivate host: #{e}"
+          end
+        end
+      end
+    end
   end
 end
+
---
+++ puppetdb_foreman.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'puppetdb_foreman'
+  s.version     = '0.0.1'
+  s.date        = '2013-03-19'
+  s.summary     = ""
+  s.description = ""
+  s.authors     = ["Daniel Lobato Garcia"]
+  s.email       = 'elobatocs@gmail.com'
+  s.files       = FileList['lib/**'].to_a + FileList['app/**'].to_a 
+  s.homepage    = 'http://www.github.com/cernops/puppetdb_foreman'
+end
