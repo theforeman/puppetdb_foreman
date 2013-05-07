9bc5d6792bc718ea1b23c0201f2b94f5556151c8
Daniel Lobato <elobatocs@gmail.com>
2013-05-07 10:26:48 +0200

    Host changed by Host::Managed in order to support foreman 1.2

--- lib/puppetdb_foreman/engine.rb
+++ lib/puppetdb_foreman/engine.rb
@@ -2,7 +2,7 @@ module PuppetdbForeman
   class Engine < ::Rails::Engine
 
     config.to_prepare do
-      Host.send :include, PuppetdbForeman::HostExtensions
+      Host::Managed.send :include, PuppetdbForeman::HostExtensions
     end
 
   end
