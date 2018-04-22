4f3f5a54128d18975ce396bb80cb991631d04ab8
Timo Goebel <mail@timogoebel.name>
2018-04-22 09:52:53 +0200

    dashboard proxy works with rails 5

--- app/controllers/puppetdb_foreman/puppetdb_controller.rb
+++ app/controllers/puppetdb_foreman/puppetdb_controller.rb
@@ -1,16 +1,38 @@
 module PuppetdbForeman
   class PuppetdbController < ApplicationController
     protect_from_forgery :except => :index
+    prepend_before_action :allow_google_fonts
 
     def index
       uri = URI.parse(Setting[:puppetdb_dashboard_address])
-      puppetdb_url, layout = case params[:puppetdb]
-                             when 'd3.v2', 'charts' then ["#{uri.path}#{request.original_fullpath}", false]
-                             when 'v3', 'metrics', 'pdb/meta/v1/version', 'pdb/meta/v1/version/latest', 'pdb/dashboard/data' then [request.original_fullpath, false]
-                             else ["#{uri.path}/index.html", true]
-                             end
-      result = Net::HTTP.get_response(uri.host, puppetdb_url, uri.port)
-      render :text => result.body, :layout => layout
+
+      if params[:puppetdb_url].blank?
+        redirect_to status: :found, action: :index, puppetdb_url: "#{uri.path}/"
+        return
+      end
+
+      if params[:puppetdb] == 'puppetdb'
+        request_url = params[:puppetdb_url]
+      elsif params[:puppetdb] == 'pdb'
+        request_url = "pdb/#{params[:puppetdb_url]}"
+      end
+      puppetdb_mapping = map_to_puppetdb_url(request_url, uri)
+      puppetdb_url = puppetdb_mapping[:path]
+
+      logger.info "PuppetDB: Proxying '#{params[:puppetdb_url]}' to '#{uri.scheme}://#{uri.host}:#{uri.port}/#{puppetdb_url}' as #{puppetdb_mapping[:content_type]}"
+
+      @result = Net::HTTP.get_response(uri.host, puppetdb_url, uri.port)
+
+      unless puppetdb_mapping[:layout]
+        respond_to do |format|
+          format.any do
+            render body: @result.body, content_type: puppetdb_mapping[:content_type], status: :ok
+          end
+          format.json do
+            render json: @result.body
+          end
+        end
+      end
     rescue SocketError
       @proxy_error = "Problem connecting to host #{uri.host} on port #{uri.port}"
       render :action => :error, :layout => true
@@ -25,6 +47,23 @@ module PuppetdbForeman
     # Override from application controller to fix issue
     def api_request?
       request.format && (request.format.json? || request.format.yaml?)
+    end
+
+    private
+
+    def map_to_puppetdb_url(request_url, uri)
+      case request_url
+      when /\.js$/
+        { path: "/#{request_url}", content_type: Mime[:js], layout: false }
+      when /^v3/, /^metrics/, /^pdb\/meta/, /^pdb\/dashboard\/data/
+        { path: "/#{request_url}", content_type: Mime[:json], layout: false }
+      else
+        { path: "#{uri.path}/index.html", content_type: Mime[:html], layout: true }
+      end
+    end
+
+    def allow_google_fonts
+      append_content_security_policy_directives(style_src: ['https://fonts.googleapis.com/'])
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get ':puppetdb(/*puppetdb_url)', :to => 'puppetdb_foreman/puppetdb#index', :puppetdb => /d3\.v2|charts|v3|puppetdb|metrics|\/pdb\/meta\/v1\/version\/latest|pdb\/meta\/v1\/version|pdb\/dashboard\/data/, :as => 'puppetdb'
+  get ':puppetdb/(*puppetdb_url)', to: 'puppetdb_foreman/puppetdb#index', constraints: { puppetdb_url: %r{.*}, puppetdb: /puppetdb|pdb/ }
 
   namespace :puppetdb_foreman do
     constraints(:id => %r{[^\/]+}) do
