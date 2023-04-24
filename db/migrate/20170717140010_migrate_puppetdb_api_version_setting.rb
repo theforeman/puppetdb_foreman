166aa9171abd9d9113174d3d6121f4ffd4e49572
Leos Stejskal <github@stejskalleos.cz>
2023-04-24 15:24:55 +0530

    Update 20170717140010_migrate_puppetdb_api_version_setting.rb

--- db/migrate/20170717140010_migrate_puppetdb_api_version_setting.rb
+++ db/migrate/20170717140010_migrate_puppetdb_api_version_setting.rb
@@ -2,11 +2,11 @@
 
 class MigratePuppetdbApiVersionSetting < ActiveRecord::Migration[4.2]
   def up
-    puppetdb_address = Setting.where(category: 'Setting::Puppetdb', name: 'puppetdb_address').first.try(:value)
+    puppetdb_address = Setting.where(category: 'Puppetdb', name: 'puppetdb_address').first.try(:value)
     return unless puppetdb_address
 
     api_version = URI.parse(puppetdb_address).path.start_with?('/pdb') ? 3 : 1
-    setting = Setting.where(category: 'Setting::Puppetdb', name: 'puppetdb_api_version').first_or_create
+    setting = Setting.where(category: 'Puppetdb', name: 'puppetdb_api_version').first_or_create
     setting.value = api_version
     setting.save
   end
