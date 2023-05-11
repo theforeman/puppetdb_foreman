theforeman/puppetdb_foreman

commit 99f4ea7b090598d95ac1a1654e4510df20a2cb13

Use the certname rather than the hostname to delete
The deactivate node requires the certname. In Foreman we track this
separately.

https://puppet.com/docs/puppetdb/7/api/wire_format/deactivate_node_format_v3.html

app/models/concerns/orchestration/puppetdb.rb
@@ -20,11 +20,12 @@ module Orchestration
     end
 
     def del_puppetdb
-      Rails.logger.info "Deactivating node in PuppetDB: #{name}"
-      ::Puppetdb.client.deactivate_node(name)
+      Rails.logger.info "Deactivating node in PuppetDB: #{certname} (#{name})"
+      ::Puppetdb.client.deactivate_node(certname)
     rescue StandardError => e
       failure format(
-        _("Failed to deactivate node %<name>s in PuppetDB: %<message>s\n "), name: name, message: e.message
+        _("Failed to deactivate node %<certname> (%<name>) in PuppetDB: %<message>\n "),
+        name: name, message: e.message, certname: certname
       ), e
     end
 

test/models/host_test.rb
@@ -26,7 +26,7 @@ class HostTest < ActiveSupport::TestCase
       end
 
       test '#del_puppetdb' do
-        ::PuppetdbClient::V4.any_instance.expects(:deactivate_node).with(host.name).returns(true)
+        ::PuppetdbClient::V4.any_instance.expects(:deactivate_node).with(host.certname).returns(true)
         host.send(:del_puppetdb)
       end
     end

commit fa49b9517fc7174db2352dec4e3b306826b7d044

Fix broken format string
Fixes: 99f4ea7b090598d95ac1a1654e4510df20a2cb13

app/models/concerns/orchestration/puppetdb.rb
@@ -24,7 +24,7 @@ module Orchestration
       ::Puppetdb.client.deactivate_node(certname)
     rescue StandardError => e
       failure format(
-        _("Failed to deactivate node %<certname> (%<name>) in PuppetDB: %<message>\n "),
+        _("Failed to deactivate node %<certname>s (%<name>s) in PuppetDB: %<message>s\n "),
         name: name, message: e.message, certname: certname
       ), e
     end