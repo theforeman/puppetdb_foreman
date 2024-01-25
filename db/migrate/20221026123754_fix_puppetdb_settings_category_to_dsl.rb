165c69ec3cb653529138588745343352f78d6809
archanaserver <ak31960466@gmail.com>
2024-01-25 23:31:34 +0530

    Fix Lint/RedundantCopDisableDirective cop

--- db/migrate/20221026123754_fix_puppetdb_settings_category_to_dsl.rb
+++ db/migrate/20221026123754_fix_puppetdb_settings_category_to_dsl.rb
@@ -2,8 +2,6 @@
 
 class FixPuppetdbSettingsCategoryToDsl < ActiveRecord::Migration[6.0]
   def up
-    # rubocop:disable Rails/SkipsModelValidations
     Setting.where(category: 'Setting::Puppetdb').update_all(category: 'Setting') if column_exists?(:settings, :category)
-    # rubocop:enable Rails/SkipsModelValidations
   end
 end
